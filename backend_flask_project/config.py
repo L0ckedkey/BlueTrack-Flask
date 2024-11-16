c0f3a86cb211cf48df6d40f1ce89e7ca672e9897
Remixingg <xxxxxremixingxxxxx@gmail.com>
2024-11-16 14:23:37 +0700

    fixing default route 3

--- backend_flask_project/config.py
+++ backend_flask_project/config.py
@@ -5,5 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SECRET_KEY = os.environ.get("SECRET_KEY", "bluetrack")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False

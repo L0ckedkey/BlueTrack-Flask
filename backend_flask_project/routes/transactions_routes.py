ef70df1ebeccc88cbb930e044a64a8fcb0f58454
Remixingg <xxxxxremixingxxxxx@gmail.com>
2024-11-18 01:31:45 +0700

    progress: transactions should be completed

--- backend_flask_project/routes/transactions_routes.py
+++ backend_flask_project/routes/transactions_routes.py
@@ -1,6 +1,8 @@
 
 from flask import Blueprint, request, jsonify
 from database.db import db
+from sqlalchemy import or_
+import numpy as np
 from model.transaction import Transaction
 from model.student import Student
 from model.node import Node
@@ -13,21 +15,20 @@ def process_data():
     try:
         # get data
         data = request.get_json()
-        required_fields = {"student_nim", "rssi_values", "esp_mac_addresses"}
+        required_fields = {"nim", "scannedDevices"}
         if not all(field in data for field in required_fields):
             return jsonify({"error": "Missing required data fields"}), 400
+        if not isinstance(data['scannedDevices'], list) or not all(isinstance(device, dict) and "macAddress" in device and "rssi" in device for device in data['scannedDevices']):
+            return jsonify({"error": "'scannedDevices' must be a list of objects containing 'macAddress' and 'rssi'"}), 400
         
         # validate data & types
-        if not isinstance(data['student_nim'], str):
-            return jsonify({"error": "Invalid 'student_nim' format"}), 400
-        if not (isinstance(data['rssi_values'], list) and len(data['rssi_values']) == 4 and all(isinstance(val, int) for val in data['rssi_values'])):
-            return jsonify({"error": "'rssi_values' must be an array of 4 integers"}), 400
-        if not (isinstance(data['esp_mac_addresses'], list) and len(data['esp_mac_addresses']) == 4 and all(isinstance(val, str) for val in data['esp_mac_addresses'])):
-            return jsonify({"error": "'esp_mac_addresses' must be an array of 4 strings"}), 400
+        if not isinstance(data['nim'], str):
+            return jsonify({"error": "Invalid 'nim' format"}), 400
+        if not isinstance(data['scannedDevices'], list) or not all(isinstance(device, dict) and "macAddress" in device and "rssi" in device for device in data['scannedDevices']):
+            return jsonify({"error": "'scannedDevices' must be a list of objects containing 'macAddress' and 'rssi'"}), 400
         
-        student_nim = data['student_nim']
-        rssi_values = data['rssi_values']
-        esp_mac_addresses = data['esp_mac_addresses']
+        student_nim = data['nim']
+        scanned_devices = data['scannedDevices']
 
         # check NIM validity
         student = Student.query.filter_by(student_nim=student_nim).first()
@@ -36,17 +37,41 @@ def process_data():
         student_id = student.id
 
         # check & get room
-        node = Node.query.filter(Node.mac_address == ','.join(esp_mac_addresses)).first()
-        if not node:
-            return jsonify({"error": "Room with provided ESP not found"}), 404
-        room_id = node.room_id
+        mac_addresses = [device['macAddress'] for device in scanned_devices]
+        result = Node.query.all()
+        matching_rows = []
+        matching_mac_addresses = []
+        rssi_values = []
+        room_id = 0
+        for row in result:
+            db_mac_addresses = row.mac_address.split(',')
+            matches = set(mac_addresses).intersection(db_mac_addresses)
+            if len(matches) >= 4:
+                matching_rows.append(row)
+                matching_mac_addresses = db_mac_addresses  # ordering/sorting
+                room_id = row.room_id
+                ordered_rssi = []
+                for mac in matching_mac_addresses:
+                    for device in scanned_devices:
+                        if device['macAddress'] == mac:
+                            ordered_rssi.append(device['rssi'])
+                            break
+                    else:
+                        ordered_rssi.append(None)
+                rssi_values.extend(ordered_rssi)
+
+        if not matching_rows:
+            return jsonify({"error": "No matching room found with ESPs' mac addresses."}), 404
+        if room_id == 0:
+            return jsonify({"error": "Error getting room_id from matched row."}), 404
 
         # AI
         try:
             predictor = PositionPredictor()
-            predicted_position = int(predictor.predict_position(rssi_values))
+            predicted_position = int(predictor.predict_position(np.array(rssi_values)))
         except Exception as e:
             return jsonify({"error": f"Prediction error: {str(e)}"}), 500
+
         
         # add to db
         transaction = Transaction(
@@ -63,7 +88,7 @@ def process_data():
             "data": {
                 "student_id": student_id,
                 "room_id": room_id,
-                "position": predicted_position
+                # "position": predicted_position
             }
         }), 201
 
